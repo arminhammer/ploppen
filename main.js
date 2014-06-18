9822aa568d6b61e04798939a23fa5f48f66b65c5
arminhammerarmin <arminhammer@gmail.com>
2014-06-17 21:42:15 -0400

    Added Peer object, and fixed peer discovery logic.

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
 /**
  * Main.js
  */
+
+// Define the class for networked peers
+function Peer(name) {
+
+	var _name = name;
+	
+	return {
+
+		// Define a sync function
+		getName : function() {
+			return _name;
+		},
+
+		setName : function (newName) {
+			_name = newName;
+		},
+
+		//name : peerName,
+
+		// Define a function with async internals
+		slowGreet : function slowGreet() {
+			setTimeout(function() {
+				console.log(message);
+			}, 1000);
+		}
+
+	};
+}
+
+var peer = Peer("Person1");
+
+console.log("Here is " + peer.getName());
+peer.setName("Person2");
+console.log("Here is 2 " + peer.getName());
+// console.log("Peer peer's name is " + peer.name);
