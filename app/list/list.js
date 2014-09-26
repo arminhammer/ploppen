9c283096b1d6ef2bb096c771e19803b8f591d57b
arminhammerarmin <arminhammer@gmail.com>
2014-09-25 23:15:11 -0400

    Added peerList service.

--- app/list/list.js
+++ app/list/list.js
@@ -2,13 +2,21 @@
 
 angular.module('node-teiler.list', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/list', {
-    templateUrl: 'list/list.html',
-    controller: 'ListCtrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/list', {
+            templateUrl: 'list/list.html',
+            controller: 'ListCtrl'
+        });
+    }])
 
-.controller('ListCtrl', [function() {
+    .controller('ListCtrl', ['$scope','peerList', function($scope, peerList) {
 
-}]);
+    }])
+    .factory('peerList', [function() {
+        var peerList = {};
+        return function(peer) {
+            if(peer != null) {
+                peerList[peer.name] = peer;
+            }
+        };
+    }]);
