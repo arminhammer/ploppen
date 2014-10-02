75dd63c60578585c8e9dce8d9e8887b3f88298f1
arminhammer <arminhammer@gmail.com>
2014-10-01 20:36:19 -0400

    Added a Gruntfile to the project for building purposes.

---
+++ Gruntfile.js
@@ -0,0 +1,373 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+
+        myApp: {
+            app: require('./bower.json').appPath || 'app',
+            dist: 'dist'
+        },
+
+        typescript: {
+            base: {
+                src: ['<%= myApp.app %>/js/{,*/}*.ts'],
+                options: {
+                    target: 'es5',
+                    sourceMap: true
+                }
+            },
+            test: {
+                src: ['test/unit/{,*/}*.ts'],
+                options: {
+                    target: 'es5',
+                    sourceMap: true
+                }
+            }
+        },
+
+        watch: {
+            ts: {
+                files: ['<%= myApp.app %>/js/{,*/}*.ts'],
+                tasks: ['typescript']
+            },
+            tsTest: {
+                files: ['test/unit/{,*/}*.ts'],
+                tasks: ['typescript:test']
+            },
+            js: {
+                files: ['<%= myApp.app %>/js/{,*/}*.js'],
+                tasks: ['newer:jshint:all'],
+                options: {
+                    livereload: true
+                }
+            },
+            jsTest: {
+                files: ['test/unit/{,*/}*.js'],
+                tasks: ['newer:jshint:test', 'karma']
+            },
+            styles: {
+                files: ['<%= myApp.app %>/css/{,*/}*.css'],
+                tasks: ['newer:copy:styles', 'autoprefixer']
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= myApp.app %>/{,*/}*.html',
+                    '.tmp/css/{,*/}*.css',
+                    '<%= myApp.app %>/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '.tmp',
+                        '<%= myApp.app %>'
+                    ]
+                }
+            },
+            test: {
+                options: {
+                    port: 9001,
+                    base: [
+                        '.tmp',
+                        'test',
+                        '<%= myApp.app %>'
+                    ]
+                }
+            },
+            dist: {
+                options: {
+                    base: '<%= myApp.dist %>'
+                }
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: [
+                'Gruntfile.js',
+                '<%= myApp.app %>/js/{,*/}*.js'
+            ],
+            test: {
+                options: {
+                    jshintrc: '.jshintrc'
+                },
+                src: ['test/unit/{,*/}*.js']
+            }
+        },
+
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= myApp.dist %>/*',
+                        '!<%= myApp.dist %>/.git*'
+                    ]
+                }]
+            },
+            server: '.tmp'
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['last 1 version']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp/css/',
+                    src: '{,*/}*.css',
+                    dest: '.tmp/css/'
+                }]
+            }
+        },
+
+        'bower-install': {
+            app: {
+                html: '<%= myApp.app %>/index.html',
+                ignorePath: '<%= myApp.app %>/'
+            }
+        },
+
+        rev: {
+            dist: {
+                files: {
+                    src: [
+                        '<%= myApp.dist %>/js/{,*/}*.js',
+                        '<%= myApp.dist %>/css/{,*/}*.css',
+                        '<%= myApp.dist %>/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                        '<%= myApp.dist %>/css/fonts/*'
+                    ]
+                }
+            }
+        },
+
+        useminPrepare: {
+            html: '<%= myApp.app %>/index.html',
+            options: {
+                dest: '<%= myApp.dist %>'
+            }
+        },
+
+        usemin: {
+            html: ['<%= myApp.dist %>/{,*/}*.html'],
+            css: ['<%= myApp.dist %>/css/{,*/}*.css'],
+            options: {
+                assetsDirs: ['<%= myApp.dist %>']
+            }
+        },
+
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= myApp.app %>/img',
+                    src: '{,*/}*.{png,jpg,jpeg,gif}',
+                    dest: '<%= myApp.dist %>/img'
+                }]
+            }
+        },
+        svgmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= myApp.app %>/img',
+                    src: '{,*/}*.svg',
+                    dest: '<%= myApp.dist %>/img'
+                }]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    removeCommentsFromCDATA: true,
+                    removeOptionalTags: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= myApp.dist %>',
+                    src: ['*.html', 'partials/{,*/}*.html'],
+                    dest: '<%= myApp.dist %>'
+                }]
+            }
+        },
+
+        ngmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp/concat/js',
+                    src: '*.js',
+                    dest: '.tmp/concat/js'
+                }]
+            }
+        },
+
+        cdnify: {
+            dist: {
+                html: ['<%= myApp.dist %>/*.html']
+            }
+        },
+
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= myApp.app %>',
+                    dest: '<%= myApp.dist %>',
+                    src: [
+                        '*.{ico,png,txt}',
+                        '.htaccess',
+                        '*.html',
+                        'partials/{,*/}*.html',
+                        'bower_components/**/*',
+                        'img/{,*/}*.{webp}',
+                        'fonts/*'
+                    ]
+                }, {
+                    expand: true,
+                    cwd: '.tmp/img',
+                    dest: '<%= myApp.dist %>/img',
+                    src: ['generated/*']
+                }]
+            },
+            styles: {
+                expand: true,
+                cwd: '<%= myApp.app %>/css',
+                dest: '.tmp/css/',
+                src: '{,*/}*.css'
+            }
+        },
+
+        concurrent: {
+            server: [
+                'copy:styles'
+            ],
+            test: [
+                'copy:styles'
+            ],
+            dist: [
+                'copy:styles',
+                'imagemin',
+                'svgmin'
+            ]
+        },
+
+        replace: {
+            development: {
+                options: {
+                    patterns: [{
+                        json: grunt.file.readJSON(
+                            'config.development.json')
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['config.js'],
+                    dest: '<%= myApp.app %>/js'
+                }]
+            },
+            azure: {
+                options: {
+                    patterns: [{
+                        json: grunt.file.readJSON('config.azure.json')
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['config.js'],
+                    dest: '<%= myApp.app %>/js'
+                }]
+            }
+        },
+
+        karma: {
+            unit: {
+                configFile: './test/karma.conf.js',
+                singleRun: true
+            }
+        }
+    });
+
+    grunt.registerTask('serve', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean:server',
+            'bower-install',
+            'concurrent:server',
+            'autoprefixer',
+            'connect:livereload',
+            'replace:development',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('server', function () {
+        grunt.log.warn(
+            'The `server` task has been deprecated. Use `grunt serve` to start a server.'
+        );
+        grunt.task.run(['serve']);
+    });
+
+    grunt.registerTask('test', [
+        'clean:server',
+        'concurrent:test',
+        'autoprefixer',
+        'connect:test',
+        'karma'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'bower-install',
+        'useminPrepare',
+        'concurrent:dist',
+        'autoprefixer',
+        'concat',
+        'ngmin',
+        'copy:dist',
+        'cdnify',
+        'cssmin',
+        'uglify',
+        'rev',
+        'usemin',
+        'htmlmin'
+    ]);
+
+    grunt.registerTask('default', [
+        'newer:jshint',
+        'test',
+        'build'
+    ]);
+};
