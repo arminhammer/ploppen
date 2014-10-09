ae9ee328b340c8f910209a5137a19e4eaa3ad405
arminhammer <arminhammer@gmail.com>
2014-10-09 15:41:53 -0400

    Reduced chunk size

--- app/components/filetransfer/filetransfer.js
+++ app/components/filetransfer/filetransfer.js
@@ -44,7 +44,7 @@ angular.module('node-teiler.filetransfer', [])
 
                                     .on('readable', function () {
                                         var chunk;
-                                        while (null !== (chunk = readableFile.read())) {
+                                        while (null !== (chunk = readableFile.read(8192))) {
                                             //console.log("chunk size: " + chunk.length);
                                             peerList[data.peername].socket.emit('file.download.data', { filename: data.filename, filedata: chunk });
                                         }
